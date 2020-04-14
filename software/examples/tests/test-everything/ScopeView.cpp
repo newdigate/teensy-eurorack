0ffe2ffddbdb8870d5f3fb3f17c216cd2740ff58
Nic Newdigate <nicnewdigate@gmail.com>
2020-04-14 08:53:59 +0100

    Create ScopeView.cpp

---
+++ software/examples/tests/test-everything/ScopeView.cpp
@@ -0,0 +1,19 @@
+#include "ScopeView.h"
+
+void ScopeView::drawScope() {
+    oscilliscope_x = oscilliscope_x + 1;
+    if (oscilliscope_x > 127) {
+        return;
+    }
+
+    _tft->drawLine(oscilliscope_x, 64 + (lastbuffer[oscilliscope_x-1] >> 8), oscilliscope_x + 1, 64 + (lastbuffer[oscilliscope_x] >> 8), _backgroundColor);
+    _tft->drawLine(oscilliscope_x, 64 + (buffer[oscilliscope_x-1] >> 8), oscilliscope_x + 1, 64 + (buffer[oscilliscope_x] >> 8), _color);
+}
+
+void ScopeView::takeBuffer() {
+    if (_getBufferFn != nullptr) {
+      memcpy(lastbuffer, buffer, 256);
+      memcpy(buffer, _getBufferFn(), 256);
+      oscilliscope_x = 0;
+    }
+}
