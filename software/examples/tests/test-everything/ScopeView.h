67ca54a6ee069118bcce7c9325b028fbd9d537bc
Nic Newdigate <nicnewdigate@gmail.com>
2020-04-14 08:53:38 +0100

    Create ScopeView.h

---
+++ software/examples/tests/test-everything/ScopeView.h
@@ -0,0 +1,48 @@
+//
+// Created by Nicholas Newdigate on 13/04/2020.
+//
+
+#ifndef ARDUINO_MIDI_WRITER_SCOPEVIEW_H
+#define ARDUINO_MIDI_WRITER_SCOPEVIEW_H
+
+#include "Arduino.h"
+#include <ST7735_t3.h> // Hardware-specific library
+
+
+#undef swap
+#include <functional>
+using namespace std;
+#undef swap
+#define swap(a, b) { typeof(a) t = a; a = b; b = t; }
+
+class ScopeView {
+public:
+    inline ScopeView(
+            ST7735_t3 &tft,
+            function<int16_t*()> getBufferFn,
+            int16_t color,
+            int16_t backgroundColor) :
+            _tft(&tft),
+            _getBufferFn(getBufferFn),
+            _backgroundColor(backgroundColor),
+            _color(color) {
+    };
+
+    void drawScope();
+    void takeBuffer();
+    uint16_t oscilliscope_x = 0;
+    
+private:
+    ST7735_t3 *_tft;
+
+    int16_t buffer[128];
+    int16_t lastbuffer[128];
+    int16_t _backgroundColor;
+    int16_t _color;
+
+    function<int16_t*()> _getBufferFn;
+};
+
+
+
+#endif //ARDUINO_MIDI_WRITER_SCOPEVIEW_H
