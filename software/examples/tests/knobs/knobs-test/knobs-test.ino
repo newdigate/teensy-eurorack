ca13fc5bd1e6d5764ffeb66f733503758029665e
Nic Newdigate <nicnewdigate@gmail.com>
2020-04-25 17:01:20 +0100

    change to read midi data from serial3 instead

--- software/examples/tests/knobs/knobs-test/knobs-test.ino
+++ software/examples/tests/knobs/knobs-test/knobs-test.ino
@@ -13,20 +13,20 @@
 #include <ST7735_t3.h> // Hardware-specific library
 #include <ST7789_t3.h> // Hardware-specific library
 #include <SPI.h>
+#include <MIDI.h>
 
 // Option 1: use any pins but a little slower
 // Note: code will detect if specified pins are the hardware SPI pins
 //       and will use hardware SPI if appropriate
 // For 1.44" and 1.8" TFT with ST7735 use
 ST7735_t3 TFT = ST7735_t3(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
-
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial3,    MIDI);
 // ----------------------------------------------
 // setup
 // ----------------------------------------------
 void setup() {
   Serial.begin(9600);
-  Serial3.begin(9600);
-  
+  MIDI.begin();   
   TFT.initR(INITR_144GREENTAB);
   TFT.setRotation(3);
   TFT.fillScreen(ST7735_BLACK);
@@ -37,13 +37,19 @@ int ypos = 0;
 void loop() {
 
   TFT.setTextSize(0);
+  if (MIDI.read())                // Is there a MIDI message incoming ?
+ {
+      TFT.printf("%2x %2x %2x %2x\n", MIDI.getType(), MIDI.getChannel(), MIDI.getData1(), MIDI.getData2() );
+      ypos ++;     
+  }
+
   
   // read from port 0, send to port 1:
   while (Serial.available()) {
     int inByte = Serial.read();
     Serial3.write(inByte);
   }
-
+/*
   // read from port 0, send to port 1:
   while (Serial3.available()) {
     char inByte = Serial3.read();
@@ -51,7 +57,7 @@ void loop() {
     TFT.print(inByte);
     if (inByte == '\n') ypos ++;    
   }
-
+*/
   if (ypos > 15) {
       TFT.fillScreen(ST7735_BLACK);
       TFT.setCursor(0,0);
